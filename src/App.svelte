28ef00a5117c5279792b901fe94cf4715a5836c8
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2024-07-30 18:56:09 +0600

    Update button text color

--- src/App.svelte
+++ src/App.svelte
@@ -63,7 +63,7 @@
         <div class="text-center py-16">
             <div class="text-8xl font-bold text-slate { processing ? 'animate animate-pulse' : '' }">{ speed }</div>
             <button on:click={testSpeed}
-                    class="mt-16 inline-flex items-center justify-center whitespace-nowrap rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-slate-800 text-slate-700 hover:bg-slate-900 px-8 py-4 text-lg text-slate-200 font-medium">
+                    class="mt-16 inline-flex items-center justify-center whitespace-nowrap rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-slate-800 text-slate-700 hover:bg-slate-900 px-8 py-4 text-lg text-white font-medium">
                 Test Speed
             </button>
         </div>
